4a8fd435865531f8847775bf1effaed98f4acdbb
MlhaelaP <mihaelapopova6@gmail.com>
2023-10-12 15:57:34 +0300

    Added MainView

---
+++ HotelReservationManager/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HotelReservationManager
+{
+    using System;
+    using System.Windows.Forms;
+
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainView());
+        }
+    }
+}
